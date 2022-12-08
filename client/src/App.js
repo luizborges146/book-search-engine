79337bb5a5d7c840ad2d075d7b68b1cde16c9662
luizborges146 <113717656+luizborges146@users.noreply.github.com>
2022-12-08 08:15:56 -0800

    Added a construct request that will attach the JWT token to every request as an  header

--- client/src/App.js
+++ client/src/App.js
@@ -4,9 +4,34 @@ import SearchBooks from './pages/SearchBooks';
 import SavedBooks from './pages/SavedBooks';
 import Navbar from './components/Navbar';
 
+import { ApolloProvider, ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
+
+const link = createHttpLink({
+  uri: '/graphql'
+});
+
+const client = new ApolloClient({
+  uri: '/graphql',
+  cache: new InMemoryCache(),
+});
+
+// Construct request middleware that will attach the JWT token to every request as an `authorization` header
+const authLink = setContext((_, { headers }) => {
+  // get the authentication token from local storage if it exists
+  const token = localStorage.getItem('id_token');
+  // return the headers to the context so httpLink can read them
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
+
 function App() {
   return (
-    <Router>
+    <Router> //
       <>
         <Navbar />
         <Switch>
